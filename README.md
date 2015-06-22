b0559ff5b14cb7a3b245618292e7c17d72ec3596
yoshiki_tanaka <yoshiki@iii-planning.com>
2015-06-22 17:05:55 +0900

    Initialize repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# どう解く会 第二回目 タクシー
+
+フォークしてくれたまえ
+
+http://nabetani.sakura.ne.jp/hena/ord30taxi/
---
+++ src/Exception/LogicException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the TripleI.taxi
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace TripleI\taxi\Exception;
+
+class LogicException extends \LogicException implements ExceptionInterface
+{
+}
